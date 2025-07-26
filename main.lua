641a04d9956d11efe280d20632f30f24a8e4b32b
bypassv5 <163147680+bypassv5@users.noreply.github.com>
2025-07-26 17:38:30 -0400

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,9 +1,1 @@
--- 🔁 Auto-Reinject Loader
-local url = "https://raw.githubusercontent.com/bypassv5/SabChecker/refs/heads/main/script.lua"
-
-if queue_on_teleport then
-    queue_on_teleport("loadstring(game:HttpGet('" .. url .. "'))()")
-end
-
--- Run immediately
-loadstring(game:HttpGet(url))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/bypassv5/SabChecker/refs/heads/main/script.lua"))()
